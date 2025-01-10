f217a5d712ab7df4a6485850c62477a33358cccd
Abiodun Sabitu <Abiodun.Sabitu@sterling.ng>
2025-01-10 13:59:48 +0100

    remove ant btn border color

--- src/components/FloatingMenu.tsx
+++ src/components/FloatingMenu.tsx
@@ -3,52 +3,56 @@ import { ConfigProvider, FloatButton } from "antd";
 import { MdMenuBook, MdNoteAdd } from "react-icons/md";
 import { RiMoonClearLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const FloatingMenu: React.FC<{
   isMenuOpen: boolean;
   setMenu: (value: boolean) => void;
-}> = ({ isMenuOpen, setMenu }) => (
-  <>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#9d4edd", // Primary color
-          // colorPrimaryHover: "#00000", // Hover color
-        },
-      }}
-    >
-      <FloatButton.Group
-        onClick={() => setMenu(!isMenuOpen)}
-        trigger="click"
-        type="primary"
-        style={{
-          insetInlineEnd: 94,
-          right: 40,
-
-          zIndex: 1200,
+}> = ({ isMenuOpen, setMenu }) => {
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#9d4edd", // Primary color
+            // colorPrimaryHover: "#00000", // Hover color
+          },
         }}
-        icon={<CgMenuGridR />}
-        // open={isMenuOpen}
       >
-        <Link to="/create-note">
-          <FloatButton
-            icon={<MdNoteAdd fill="#9d4edd" />}
-            tooltip={<div>Add Note</div>}
-          />
-        </Link>
-        <Link to="/all-notes">
-          <FloatButton
-            icon={<MdMenuBook fill="#9d4edd" />}
-            tooltip={<div>All Notes</div>}
-          />
-        </Link>
-        <FloatButton
-          icon={<RiMoonClearLine fill="#9d4edd" />}
-          tooltip={<div>Theme</div>}
-        />
-      </FloatButton.Group>
-    </ConfigProvider>
-  </>
-);
+        <FloatButton.Group
+          onClick={() => setMenu(!isMenuOpen)}
+          trigger="click"
+          type="primary"
+          style={{
+            insetInlineEnd: 94,
+            right: 40,
+
+            zIndex: 1200,
+          }}
+          icon={<CgMenuGridR />}
+          // open={isMenuOpen}
+        >
+          <Link to="/create-note">
+            <FloatButton
+              icon={<MdNoteAdd fill="#9d4edd" />}
+              tooltip={<div>Add Note</div>}
+            />
+          </Link>
+          <Link to="/all-notes">
+            <FloatButton
+              icon={<MdMenuBook fill="#9d4edd" />}
+              tooltip={<div>All Notes</div>}
+            />
+          </Link>
+          {/* <FloatButton
+            onClick={toggleTheme}
+            icon={<RiMoonClearLine fill="#9d4edd" />}
+            tooltip={<div>Theme</div>}
+          /> */}
+        </FloatButton.Group>
+      </ConfigProvider>
+    </>
+  );
+};
 
 export default FloatingMenu;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,59 @@
+import { useEffect, useState } from "react";
+import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
 import { RiStickyNoteAddFill } from "react-icons/ri";
 
 const Header: React.FC<{}> = () => {
+  const [theme, setTheme] = useState<"light" | "dark">(
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  );
+
+  // Function to toggle between light and dark modes
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light"; // Determine the new theme
+    setTheme(newTheme); // Update the state with the new theme
+    document.body.className = newTheme + "-mode"; // Apply the class to the <body>
+  };
+
+  // Apply the current theme when the component mounts or when the theme state changes
+  useEffect(() => {
+    document.body.className = theme + "-mode";
+  }, [theme]);
+
   return (
     <header id="header">
-      <div className="logo">
-        <i>Cabinote </i> &nbsp;
-        <RiStickyNoteAddFill fill="#E8505B" />
+      <div>
+        <div className="logo" style={{ padding: 0, margin: 0 }}>
+          <i>Cabinote </i> &nbsp;
+          <RiStickyNoteAddFill fill="#E8505B" />
+          <br />
+        </div>
+        <div
+          style={{
+            padding: 0,
+            marginBottom: 15,
+            fontSize: "10px",
+            textAlign: "center",
+          }}
+        >
+          Big ideas start small
+        </div>
       </div>
 
-      <i
+      <div
         style={{
           marginTop: 5,
           marginLeft: 5,
           color: "rgba(68, 67, 67, 0.77)",
           fontWeight: "bold",
         }}
+        onClick={toggleTheme}
       >
-        <small>Big ideas start small</small>
-      </i>
+        {theme === "light" ? (
+          <MdDarkMode size={24} cursor="pointer" />
+        ) : (
+          <MdOutlineDarkMode fill="white" size={24} cursor="pointer" />
+        )}
+      </div>
     </header>
   );
 };
